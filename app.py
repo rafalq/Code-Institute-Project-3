19fcb37f761bfec694f20dd76a36c9e6df358a8c
Rafal Kruszewski <kruszewskirafael@gmail.com>
2019-11-14 19:19:57 +0000

    corrected, ready to push

--- app.py
+++ app.py
@@ -5,35 +5,36 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.secret_key = 'some_secret'
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-app.config["MONGO_DBNAME"] = 'online_cookbook'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_DBNAME"] = "online_cookbook"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI", 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
-@app.route('/no review', methods=['POST', 'GET'])
-def no_review():
+@app.route('/recipes_without_review', methods=['POST', 'GET'])
+def recipes_without_review():
     recipes = mongo.db.recipes.find({'votes': {'$exists' : False} })
     count_cuisines = mongo.db.recipes.aggregate([ {'$group': { '_id': {"cuisine": "$cuisine"}, 'count': {'$sum': 1} } } ])
-    return render_template('search.html',recipes=recipes, count_cuisines=count_cuisines)
+    return render_template('recipes.html',recipes=recipes, count_cuisines=count_cuisines)
     
-@app.route('/search', methods=['POST'])    
-def search():
+@app.route('/recipes', methods=['POST'])    
+def recipes():
     search_title = request.form.get('search_name')
     mongo.db.recipes.create_index([('$**', 'text')])
     recipes = mongo.db.recipes.find({'$text': {'$search': search_title}})
-    return render_template('search.html', recipes=recipes, search_title=search_title)
+    return render_template('recipes.html', recipes=recipes, search_title=search_title)
 
-@app.route('/rank_edit', methods=['POST'])
-def rank_edit():
+@app.route('/reviewed_recipes', methods=['POST'])
+def reviewed_recipes():
     search_score = int(request.form.get('search_rate'))
     recipes = mongo.db.recipes.find({'votes': {'$exists' : True} })
     count_cuisines = mongo.db.recipes.aggregate([ {'$group': { '_id': {"cuisine": "$cuisine"}, 'count': {'$sum': 1} } } ])
-    return render_template('rank_edit.html', recipes=recipes, search_score=search_score, count_cuisines=count_cuisines)
+    return render_template('reviewed_recipes.html', recipes=recipes, search_score=search_score, count_cuisines=count_cuisines)
 
 @app.route('/')
-def cookbook():
+@app.route('/home')
+def home():
     recipes = mongo.db.recipes.find().sort('recipe_title')
     cuisines_all = mongo.db.recipes.find({},{'cuisine':1, '_id':0}).count()
     count_cuisines = mongo.db.recipes.aggregate([ {'$group': { '_id': {"cuisine": "$cuisine"}, 'count': {'$sum': 1} } } ])
@@ -68,12 +69,12 @@ def cookbook():
             if(recipe["vegeterian"] is not None):
                 veg_counter += 1
     
-    return render_template('cookbook.html', recipes=recipes, cuisines_all=cuisines_all,count_cuisines=count_cuisines, five_counter=five_counter, four_counter=four_counter, three_counter=three_counter, two_counter=two_counter, one_counter=one_counter, no_review_counter=no_review_counter, veg_counter=veg_counter, count_meals=count_meals)
+    return render_template('home.html', recipes=recipes, cuisines_all=cuisines_all,count_cuisines=count_cuisines, five_counter=five_counter, four_counter=four_counter, three_counter=three_counter, two_counter=two_counter, one_counter=one_counter, no_review_counter=no_review_counter, veg_counter=veg_counter, count_meals=count_meals)
 
-@app.route('/veg')
-def veg():
+@app.route('/vegeterian_recipes')
+def vegeterian_recipes():
     recipes = mongo.db.recipes.find()
-    return render_template('veg.html', recipes=recipes)
+    return render_template('vegeterian_recipes.html', recipes=recipes)
     
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
@@ -87,11 +88,11 @@ def review_recipe(recipe_id):
     recipe.update({'_id': ObjectId(recipe_id)},{'$inc': {"score": vote_int, "votes": 1}})
     if request.method == 'POST':
         flash('Thanks, we have received your vote!')
-    return redirect(url_for('cookbook'))
+    return redirect(url_for('home'))
  
-@app.route('/add_recipe')
-def add_recipe():
-    return render_template('add_recipe.html')
+@app.route('/share_your_recipe')
+def share_your_recipe():
+    return render_template('share_your_recipe.html')
  
 @app.route('/insert_recipe', methods=['POST']) 
 def insert_recipe ():
@@ -99,16 +100,16 @@ def insert_recipe ():
     recipes.insert_one(request.form.to_dict())
     if request.method == 'POST':
         flash('Thanks, {}, we have received your recipe!'.format(request.form['author']))
-    return redirect(url_for('cookbook'))
+    return redirect(url_for('home'))
     
-@app.route('/for_update/<recipe_id>', methods=['POST','GET'])
-def for_update(recipe_id):
+@app.route('/update_recipe/<recipe_id>', methods=['POST','GET'])
+def update_recipe(recipe_id):
     recipes = mongo.db.recipes.find()
     the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    return render_template('for_update.html', recipes=recipes, recipe=the_recipe)
+    return render_template('update_recipe.html', recipes=recipes, recipe=the_recipe)
 
-@app.route('/update_recipe/<recipe_id>', methods=['POST'])
-def update_recipe(recipe_id):
+@app.route('/update/<recipe_id>', methods=['POST'])
+def update(recipe_id):
     recipe = mongo.db.recipes
     recipe.update({'_id': ObjectId(recipe_id)},{
         '$set':{
@@ -129,13 +130,13 @@ def update_recipe(recipe_id):
         })
     if request.method == 'POST':
         flash('Thanks, {}, your recipe has been updated!'.format(request.form['author']))        
-    return redirect(url_for('cookbook'))
+    return redirect(url_for('home'))
 
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     recipe = mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
     flash('The recipe has been deleted!')
-    return redirect(url_for('cookbook'))   
+    return redirect(url_for('home'))   
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),port=os.environ.get('PORT'),debug=False)
